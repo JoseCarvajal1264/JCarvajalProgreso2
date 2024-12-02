50ed94386d2a772d6f0dabf634b7522553926c5e
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-12-02 07:14:09 -0500

    Agregar archivos de proyecto.

---
+++ JCarvajalProgreso2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JCarvajalProgreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
