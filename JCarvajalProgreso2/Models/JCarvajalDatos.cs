ddea5c4a4f5bae266ed3ac964c1e9d67238b6fcf
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-12-02 08:03:26 -0500

    DATOS RECARGA

---
+++ JCarvajalProgreso2/Models/JCarvajalDatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCarvajalProgreso2.Models
+{
+    internal class JCarvajalDatos
+    {
+        public int Numero {  get; set; }
+        public string Nombre { get; set; }
+    }
+}
