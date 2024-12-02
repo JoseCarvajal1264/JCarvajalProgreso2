3683748303155222138d9788f9f98eb62b5d7646
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-12-02 07:28:29 -0500

    grid sin cambio de estilo

---
+++ JCarvajalProgreso2/JCarvajalPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace JCarvajalProgreso2;
+
+public partial class JCarvajalPage1 : ContentPage
+{
+	public JCarvajalPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ JCarvajalProgreso2/JCarvajalPage2.xaml.cs
@@ -0,0 +1,9 @@
+namespace JCarvajalProgreso2;
+
+public partial class JCarvajalPage2 : ContentPage
+{
+	public JCarvajalPage2()
+	{
+		InitializeComponent();
+	}
+}
